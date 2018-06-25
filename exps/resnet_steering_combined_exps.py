c14701625717dae8a9771485a659da33eebf4b9b
Yuhuang Hu <duguyue100@gmail.com>
2018-06-25 11:12:41 +0200

    update combined models prediction

--- exps/resnet_steering_combined_exps.py
+++ exps/resnet_steering_combined_exps.py
@@ -5,15 +5,16 @@ Email : duguyue100@gmail.com
 """
 from __future__ import print_function
 import os
-import cPickle as pickle
 
 from sacred import Experiment
 
 import numpy as np
 import h5py
-from keras.models import load_model
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import CSVLogger
 
 import spiker
+from spiker.models import resnet
 from spiker.data import ddd17
 
 exp = Experiment("ResNet - Steering - Experiment")
@@ -36,6 +37,11 @@ def resnet_exp(model_name, data_name, channel_id, stages, blocks, filter_list,
     """Perform ResNet experiment."""
     model_path = os.path.join(spiker.HOME, "data", "exps", "ral-exps",
                               model_name)
+    if not os.path.isdir(model_path):
+        os.makedirs(model_path)
+    else:
+        raise ValueError("[MESSAGE] This experiment has been done before."
+                         " Create a new config model if you need.")
     model_file_base = os.path.join(model_path, model_name)
 
     # print model info
@@ -55,23 +61,57 @@ def resnet_exp(model_name, data_name, channel_id, stages, blocks, filter_list,
     dataset = h5py.File(data_path, "r")
 
     if channel_id != 2:
+        X_train = dataset["train_data"][
+            :, :, :, channel_id][()][..., np.newaxis].astype("float32")/255.
         X_test = dataset["test_data"][
             :, :, :, channel_id][()][..., np.newaxis].astype("float32")/255.
     else:
+        X_train = dataset["train_data"][()].astype("float32")/255.
         X_test = dataset["test_data"][()].astype("float32")/255.
 
+    num_samples = X_train.shape[0]
+
+    Y_train = dataset["train_target"][()]
     Y_test = dataset["test_target"][()]
 
     dataset.close()
 
+    print("[MESSAGE] Number of samples %d" % (num_samples))
+    print("[MESSAGE] Number of train samples %d" % (X_train.shape[0]))
     print("[MESSAGE] Number of test samples %d" % (X_test.shape[0]))
 
+    # setup image shape
+    input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
+
     # Build model
+    model = resnet.resnet_builder(
+        model_name=model_name, input_shape=input_shape,
+        batch_size=batch_size,
+        filter_list=filter_list, kernel_size=(3, 3),
+        output_dim=1, stages=stages, blocks=blocks,
+        bottleneck=False, network_type="regress")
+
+    model.summary()
+    model.compile(loss='mean_squared_error',
+                  optimizer="adam",
+                  metrics=["mse"])
     print ("[MESSAGE] Model is compiled.")
     model_file = model_file_base + "-best.hdf5"
-    model = load_model(model_file)
+    checkpoint = ModelCheckpoint(model_file,
+                                 monitor='val_mean_squared_error',
+                                 verbose=1,
+                                 save_best_only=True,
+                                 mode='min')
 
-    Y_predict = model.predict(X_test)
+    csv_his_log = os.path.join(model_path, "csv_history.log")
+    csv_logger = CSVLogger(csv_his_log, append=True)
 
-    with open(model_file_base+"-prediction.pkl", "wb") as f:
-        pickle.dump([Y_test, Y_predict], f)
+    callbacks_list = [checkpoint, csv_logger]
+
+    # training
+    model.fit(
+        x=X_train, y=Y_train,
+        batch_size=batch_size,
+        epochs=nb_epoch,
+        validation_data=(X_test, Y_test),
+        callbacks=callbacks_list)
--- scripts/ral_create_figures.py
+++ scripts/ral_create_figures.py
@@ -21,101 +21,105 @@ channel_options = {
     "dvs": 0}
 
 
-def get_subset_results(exps_root_path, lighting, num_exps,
+def get_subset_results(exps_root_path, lighting,
                        get_eva=False):
     result_collector = {}
     rmse_collector = {}
     for cond in conditions:
         rmse_collector[cond] = []
         rmse_collector[cond+"_eva"] = []
-        for idx in range(1, num_exps+1):
-            model_type = "%s-%d-%s" % (lighting, idx, cond)
-            result_collector[model_type] = {
-                "curves": [],
-                "mins": [],
-                "rmses": [],
-                "eva": [],
-                "true_var": []}
-            for trail_idx in range(1, num_trails+1):
-                # construct file name
-                model_base = \
-                    "steering-"+model_type+"-%d" % (trail_idx)
-                model_path = os.path.join(
-                    exps_root_path, model_base, "csv_history.log")
+        #  for idx in range(1, num_exps+1):
+        model_type = "%s-%s" % (lighting, cond)
+        result_collector[model_type] = {
+            "curves": [],
+            "mins": [],
+            "rmses": [],
+            "eva": [],
+            "true_var": []}
+        for trail_idx in range(1, num_trails+1):
+            # construct file name
+            model_base = \
+                model_type+"-%d" % (trail_idx)
+            model_path = os.path.join(
+                exps_root_path, model_base, "csv_history.log")
 
-                # read result
-                if get_eva is True:
-                    result_path = os.path.join(
-                        exps_root_path, model_base,
-                        model_base+"-prediction.pkl")
-                    with open(result_path, "rb") as f:
-                        data = pickle.load(f)
+            # read result
+            if get_eva is True:
+                result_path = os.path.join(
+                    exps_root_path, model_base,
+                    model_base+"-prediction.pkl")
+                with open(result_path, "rb") as f:
+                    data = pickle.load(f)
 
-                    Y_true = data[0]
-                    Y_predict = data[1][:, 0]
+                Y_true = data[0]
+                Y_predict = data[1][:, 0]
 
-                    plt.figure()
-                    plt.plot(Y_true)
-                    plt.plot(Y_predict)
-                    plt.title(model_base)
-                    plt.show()
+                plt.figure()
+                plt.plot(Y_true)
+                plt.plot(Y_predict)
+                plt.title(model_base)
+                plt.show()
 
-                    eva_score = 1-np.var(Y_true-Y_predict)/np.var(Y_true)
-                    result_collector[model_type]["y_true"] = Y_true
-                    result_collector[model_type]["y_pred"] = Y_predict
-                    result_collector[model_type]["eva"].append(eva_score)
-                    result_collector[model_type]["true_var"].append(
-                        np.std(Y_true))
-                    rmse_collector[cond+"_eva"].append(eva_score)
+                eva_score = 1-np.var(Y_true-Y_predict)/np.var(Y_true)
+                result_collector[model_type]["y_true"] = Y_true
+                result_collector[model_type]["y_pred"] = Y_predict
+                result_collector[model_type]["eva"].append(eva_score)
+                result_collector[model_type]["true_var"].append(
+                    np.std(Y_true))
+                rmse_collector[cond+"_eva"].append(eva_score)
 
-                # read loss
-                curve = np.loadtxt(
-                    model_path, delimiter=",", skiprows=1)
-                assert curve.shape[0] == 200
-                result_collector[model_type]["curves"].append(curve)
-                min_mse = np.min(curve[:, 4])
-                min_rmse = np.sqrt(min_mse)*180/np.pi
-                result_collector[model_type]["mins"].append(min_mse)
-                result_collector[model_type]["rmses"].append(min_rmse)
-                rmse_collector[cond].append(min_rmse)
-            # print 5 trails of each experiments
-            if get_eva is False:
-                print ("%s - RMSE mean %.6f %.6f std %.6f"
-                       % (model_type,
-                          np.mean(result_collector[model_type]["rmses"]),
-                          np.std(result_collector[model_type]["rmses"])))
-            else:
-                print ("%s - EVA mean %.6f %.6f, std %.6f"
-                       % (model_type,
-                          np.mean(result_collector[model_type]["eva"]),
-                          np.std(result_collector[model_type]["eva"]),
-                          np.mean(result_collector[model_type]["true_var"])))
-        # print for each condition
+            # read loss
+            curve = np.loadtxt(
+                model_path, delimiter=",", skiprows=1)
+            #  assert curve.shape[0] == 200
+            result_collector[model_type]["curves"].append(curve)
+            min_mse = np.min(curve[:, 4])
+            min_rmse = np.sqrt(min_mse)*180/np.pi
+            result_collector[model_type]["mins"].append(min_mse)
+            result_collector[model_type]["rmses"].append(min_rmse)
+            rmse_collector[cond].append(min_rmse)
+
+        # print 5 trails of each experiments
         if get_eva is False:
             print ("%s - RMSE mean %.6f %.6f"
-                   % (cond, np.mean(rmse_collector[cond]),
-                      np.std(rmse_collector[cond])))
+                   % (model_type,
+                      np.mean(result_collector[model_type]["rmses"]),
+                      np.std(result_collector[model_type]["rmses"])))
         else:
-            print ("%s - EVA - mean %.6f %.6f"
-                   % (cond, np.mean(rmse_collector[cond+"_eva"]),
-                      np.std(rmse_collector[cond+"_eva"])))
+            print ("%s - EVA mean %.6f %.6f, std %.6f"
+                   % (model_type,
+                      np.mean(result_collector[model_type]["eva"]),
+                      np.std(result_collector[model_type]["eva"]),
+                      np.mean(result_collector[model_type]["true_var"])))
+    # print for each condition
+    #  if get_eva is False:
+    #      print ("%s - RMSE mean %.6f %.6f"
+    #             % (cond, np.mean(rmse_collector[cond]),
+    #                np.std(rmse_collector[cond])))
+    #  else:
+    #      print ("%s - EVA - mean %.6f %.6f"
+    #             % (cond, np.mean(rmse_collector[cond+"_eva"]),
+    #                np.std(rmse_collector[cond+"_eva"])))
 
     return result_collector, rmse_collector
 
 
-def get_results(exps_root_path, get_eva=True):
+def get_results(exps_root_path, get_eva=False):
     """Get results."""
 
     night_results, night_rmse = get_subset_results(
-        exps_root_path, "night", 15, get_eva=get_eva)
+        exps_root_path, "night", get_eva=get_eva)
     day_results, day_rmse = get_subset_results(
-        exps_root_path, "day", 15, get_eva=get_eva)
+        exps_root_path, "day", get_eva=get_eva)
+    all_results, all_rmse = get_subset_results(
+        exps_root_path, "all", get_eva=get_eva)
 
     rmse_collector = {}
     for cond in conditions:
-        rmse_collector[cond] = night_rmse[cond]+day_rmse[cond]
-        rmse_collector[cond+"_eva"] = night_rmse[cond+"_eva"] + \
-            day_rmse[cond+"_eva"]
+        rmse_collector[cond] = night_rmse[cond]+day_rmse[cond]+all_rmse[cond]
+        if get_eva is True:
+            rmse_collector[cond+"_eva"] = night_rmse[cond+"_eva"] + \
+                day_rmse[cond+"_eva"]+all_rmse[cond+"_eva"]
         if get_eva is False:
             print ("All experiments - RMSE - %s - %.6f %.6f"
                    % (cond, np.mean(rmse_collector[cond]),
